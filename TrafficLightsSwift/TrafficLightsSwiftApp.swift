9c19173102158418e2f0eeb11cf6c79d48937746
iForneu <ikorotaevkirill@gmail.com>
2022-01-09 13:16:54 +0300

    Initial Commit

---
+++ TrafficLightsSwift/TrafficLightsSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightsSwiftApp.swift
+//  TrafficLightsSwift
+//
+//  Created by Kirill Korotaev on 09.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightsSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
